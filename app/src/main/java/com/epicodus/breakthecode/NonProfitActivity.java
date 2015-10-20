f82d84a5a85055ba0ce0cf10bc050988190513ff
Stephany Garcia <seg2136@gmail.com>
2015-10-20 14:05:48 -0700

    App has intents and click listeners

---
+++ app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.breakthecode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NonProfitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_non_profit);
+    }
+}
