53307a4dee12e8a2995ff05b69f2e1d8a7667c13
Stephany Garcia <seg2136@gmail.com>
2015-10-21 13:56:06 -0700

    Buttons to home page appear on all activities

--- app/src/main/java/com/epicodus/breakthecode/EnrollActivity.java
+++ app/src/main/java/com/epicodus/breakthecode/EnrollActivity.java
@@ -1,15 +1,30 @@
 package com.epicodus.breakthecode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class EnrollActivity extends AppCompatActivity {
+
+    private Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
+
+        mHomeButton = (Button) findViewById(R.id.homeButton);
+
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EnrollActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
+++ app/src/main/java/com/epicodus/breakthecode/NonProfitActivity.java
@@ -1,15 +1,30 @@
 package com.epicodus.breakthecode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class NonProfitActivity extends AppCompatActivity {
+
+    private Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_profit);
+
+        mHomeButton = (Button) findViewById(R.id.homeButton);
+
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NonProfitActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
